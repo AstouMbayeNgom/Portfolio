579849e8f6e65f7c3ed68f137857f19e87a6dba8
AstouMbayeNgom <astoumbayengom1999@gmail.com>
2024-12-26 18:23:11 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemple de JavaScript pour envoyer un formulaire de contact
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Merci pour votre message ! Nous vous répondrons bientôt.");
+});
